629b4101bba19062c270a23480db0feddfdf76b1
R055 <rectoross@gmail.com>
2023-06-13 13:33:54 +0800

    merge

--- administrator/admin/editAttendance.php
+++ administrator/admin/editAttendance.php
@@ -1,22 +1,12 @@
 <?php
 // if (Input::exists()) {
 // 			$book = new Books();
-<<<<<<< HEAD
-
-=======
-			
->>>>>>> b403761475987c2cd495122db6e2a36883a2df3d
 //             try {
 //                 $book->update(array(
 // 					'bookTitle' => Input::get('bookTitle'),
 // 					'author' => Input::get('author'),
 // 					'datePublished' => Input::get('datePublished'),
 //                 ), $_GET['id']);
-<<<<<<< HEAD
-
-=======
-			
->>>>>>> b403761475987c2cd495122db6e2a36883a2df3d
 // 			Session::flash('Updated', 'Book Info has been successfully updated.');
 // 			Redirect::to('admin.php?action=listBooks');
 //             } catch(Exception $e) {
@@ -69,19 +59,11 @@
                     <div class="col-xs-12">
                         <div class="box box-primary">
                             <div class="box-header">
-<<<<<<< HEAD
                                 <h3 class="box-title">Edit Attendance</h3>
                                 <div class="pull-right box-tools">
                                     <button class="btn btn-primary btn-sm" data-widget='collapse' data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
                                     <button class="btn btn-primary btn-sm" data-widget='remove' data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
                                 </div><!-- /. tools -->
-=======
-                                <h3 class="box-title">Edit Attendance</h3>    
-                                <div class="pull-right box-tools">
-                                    <button class="btn btn-primary btn-sm" data-widget='collapse' data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
-                                    <button class="btn btn-primary btn-sm" data-widget='remove' data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
-                                </div><!-- /. tools -->                                
->>>>>>> b403761475987c2cd495122db6e2a36883a2df3d
                             </div><!-- /.box-header -->
                             <div class="box-body">
 
@@ -93,11 +75,7 @@
 											<div class="form-group">
 												<div class="row">
 													<div class="col-lg-8 col-md-8">
-<<<<<<< HEAD
 														<input type="text" class="form-control" id="STUDENTID" name="STUDENTID" value="">
-=======
-														<input type="text" class="form-control" id="isbn" name="isbn" value="">
->>>>>>> b403761475987c2cd495122db6e2a36883a2df3d
 													</div>
 
 												</div>
@@ -107,19 +85,11 @@
 
                                     <div class="row">
 										<div class="col-lg-6 col-md-6">
-<<<<<<< HEAD
 											<label class="control-label" for="isbn"><font color="#EC0003">*</font>Date</label>
 											<div class="form-group">
 												<div class="row">
 													<div class="col-lg-8 col-md-8">
 														<input type="text" class="form-control" id="TIMEIN" name="TIMEIN" value="">
-=======
-											<label class="control-label" for="isbn"><font color="#EC0003">*</font> Name</label>
-											<div class="form-group">
-												<div class="row">
-													<div class="col-lg-8 col-md-8">
-														<input type="text" class="form-control" id="isbn" name="isbn" value="">
->>>>>>> b403761475987c2cd495122db6e2a36883a2df3d
 													</div>
 
 												</div>
@@ -129,32 +99,19 @@
 
                                     <div class="row">
 										<div class="col-lg-6 col-md-6">
-<<<<<<< HEAD
 											<label class="control-label" for="isbn"><font color="#EC0003">*</font>Attendance</label>
 											<div class="form-group">
 												<div class="row">
 													<div class="col-lg-8 col-md-8">
 														<input type="text" class="form-control" id="isbn" name="STATUS" value="">
-=======
-											<label class="control-label" for="isbn"><font color="#EC0003">*</font> Name</label>
-											<div class="form-group">
-												<div class="row">
-													<div class="col-lg-8 col-md-8">
-														<input type="text" class="form-control" id="isbn" name="isbn" value="">
->>>>>>> b403761475987c2cd495122db6e2a36883a2df3d
 													</div>
 
 												</div>
 											</div>
 										</div>
 									</div>
-<<<<<<< HEAD
 
 
-=======
-                                   
-								
->>>>>>> b403761475987c2cd495122db6e2a36883a2df3d
                                     <div class="clearfix"></div><hr />
                                     <div class="form-actions">
                                         <input type="hidden" name="token" value="">
@@ -177,11 +134,7 @@
 															<form enctype="multipart/form-data" method="post" action="editBookQR.php">
 																<label class="control-label" for="newIsbn"><font color="#EC0003">*</font> International Stardand Book Number</label>
 																<div class="form-group">
-<<<<<<< HEAD
 																	<input type="text" class="form-control" id="newIsbn" name="newIsbn" value="">
-=======
-																	<input type="text" class="form-control" id="newIsbn" name="newIsbn" value="">													
->>>>>>> b403761475987c2cd495122db6e2a36883a2df3d
 																</div>
 																<input type="hidden" name="newID" value="">
 														</div>
@@ -193,11 +146,7 @@
 														</div>
 													</div>
 													</div>
-<<<<<<< HEAD
 								<?php ?>
-=======
-								<?php ?>                 
->>>>>>> b403761475987c2cd495122db6e2a36883a2df3d
                             </div><!-- /.box-body -->
                         </div><!-- /.box -->
 
@@ -205,11 +154,6 @@
                 </section><!-- /.content -->
             </aside><!-- /.right-side -->
         </div><!-- ./wrapper -->
-<<<<<<< HEAD
-
-=======
-                    
->>>>>>> b403761475987c2cd495122db6e2a36883a2df3d
 <!-- jQuery 2.0.2 -->
 <script src="styles/admin/js/jquery.min.js"></script>
 <!-- Bootstrap -->
@@ -219,11 +163,7 @@
 <!-- AdminLTE App -->
 <script src="styles/admin/js/AdminLTE/app.js" type="text/javascript"></script>
 <!-- Bootstrap Validator JS -->
-<<<<<<< HEAD
 <script src="styles/admin/js/bootstrapValidator.min.js"></script>
-=======
-<script src="styles/admin/js/bootstrapValidator.min.js"></script>    
->>>>>>> b403761475987c2cd495122db6e2a36883a2df3d
 <script type="text/javascript">
 	$(document).ready(function() {
         var validator = $("#editUser").bootstrapValidator({
@@ -248,8 +188,4 @@
     });
 </script>
 </body>
-<<<<<<< HEAD
 </html>
-=======
-</html>
->>>>>>> b403761475987c2cd495122db6e2a36883a2df3d
