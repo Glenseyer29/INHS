f53e9a31751cd06c614a01d1e031033b844ce059
R055 <rectoross@gmail.com>
2023-06-13 15:41:20 +0800

    button fixed

--- administrator/admin/attendanceRecord.php
+++ administrator/admin/attendanceRecord.php
@@ -111,7 +111,7 @@
                                 <button type="submit" name="delete" class="btn btn-xs btn-danger" >DELETE</button>
                               </form>
 
-                              <form method="POST" action="admin.php?action=update" style="display:inline">
+                              <form method="POST" action="admin.php?action=editAttendance" style="display:inline">
                               <input type="hidden" name="ID" value="<?php echo $row['ID'] ?>">
                               <button type="submit" name="update" class="btn btn-xs btn-primary" >EDIT</button>
                               </form>
